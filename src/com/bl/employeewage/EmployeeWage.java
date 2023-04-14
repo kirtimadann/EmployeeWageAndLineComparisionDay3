a4c1463cece354d1ee0b1af8edeaef7d9708a701
lunainfotec <kirtimadan1997@gmail.com>
2023-04-15 00:28:40 +0530

    added : Employee wage Part time

--- src/com/bl/employeewage/EmployeeWage.java
+++ src/com/bl/employeewage/EmployeeWage.java
@@ -1,18 +1,26 @@
 package com.bl.employeewage;
 
 public class EmployeeWage {
+
     public static void main(String[] args) {
-        int IS_FULL_TIME = 1;
+
+
+        int IS_FULL_TIME = 2;
+        int IS_Part_TIME = 1;
         int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
 
 
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
 
         if (empCheck == IS_FULL_TIME) {
 
             empHrs = 8;
+        } else if (empCheck == IS_Part_TIME) {
+
+            empHrs =4;
+
         } else
 
             empHrs = 0;
@@ -21,7 +29,5 @@ public class EmployeeWage {
 
         System.out.println("Emp Wage:" + empWage);
 
-
-
-       }
+    }
 }
